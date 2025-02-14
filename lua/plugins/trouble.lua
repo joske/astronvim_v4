6e6f1f34dd31ab2c11ad24c952c451eeb49bb2bc
Jos Dehaes <jos.dehaes@gmail.com>
2025-02-14 12:21:24 +0100

    add mini.icons

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,6 +1,6 @@
 return {
   "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = { "nvim-tree/nvim-web-devicons", "echasnovski/mini.icons" },
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
